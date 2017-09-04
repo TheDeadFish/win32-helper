b239fcace84a6e51e5804b10c288e0f385e6792c
TheDeadFish <michaelstamper1@gmail.com>
2017-09-04 06:48:50 +0000

    1. listView: replaced ANSI with UTF8 2. listView: new function, listView_getFocus

--- src/listView.cc
+++ src/listView.cc
@@ -1,11 +1,15 @@
 // listview helpers - include lines
 void WINAPI lstView_insColumn(HWND hList, int index, int width, const char* name);
-void WINAPI lstView_iosItem(HWND hList, int item, int subItem, LVITEMA* lvi);
-void WINAPI lstView_iosItem(HWND hList, int item, int subItem, LVITEMW* lvi);
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const char* text);
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const char* text, LPARAM data);
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const WCHAR* text);
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const WCHAR* text, LPARAM data);
+
+
+#define _LSTVW_F1_(CT, LT) \
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text, int iSub, LT* lvi, int mask); \
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text); \
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text, LPARAM data); \
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text, LPARAM data, int img); \
+int WINAPI lstView_iosText(HWND hList, int item, int iSub, const CT* text); 
+_LSTVW_F1_(char, LVITEMA); _LSTVW_F1_(WCHAR, LVITEMW);
+
 void WINAPI lstView_iosInt(HWND hList, int item, int subItem, int val);
 void WINAPI lstView_iosInt(HWND hList, int item, int subItem, int val, LPARAM data);
 void WINAPI lstView_setData(HWND hList, int item, LPARAM data);
@@ -19,6 +23,7 @@ void WINAPI listView_setCurSel(HWND hList, int item);
 int WINAPI listView_getSel(HWND hList, int item);
 int WINAPI listView_nextSel(HWND hList, int iStart);
 void WINAPI listView_setSel(HWND hList, int item, BOOL bSelect);
+int WINAPI listView_getFocus(HWND hList);
 void WINAPI listView_setFocus(HWND hList, int item);
 
 #define lstView_insText(a,b,c) lstView_iosText(a,(b)|0x80000000,0,c)
@@ -31,26 +36,44 @@ void WINAPI lstView_insColumn(HWND hList, int index, int width, const char* name
 	LVCOLUMNA LvCol; LvCol.mask=LVCF_TEXT|LVCF_WIDTH|LVCF_SUBITEM;
 	LvCol.cx=width;	LvCol.pszText=(char*)name;
 	SendMessage(hList,LVM_INSERTCOLUMNA,index,(LPARAM)&LvCol); }
-
-#define lstView_iosItem_(set, ins, type) \
-void WINAPI lstView_iosItem(HWND hList, int item, int subItem, type* lvi) { \
-	lvi->iSubItem = subItem; int msg = set;	if((subItem == 0)&&(((item < 0) \
-	&&(item &= INT_MAX, 1))||(item >= ListView_GetItemCount(hList)))) msg = ins; \
-	lvi->iItem = item; SendMessage(hList, msg, 0, (LPARAM)lvi); asm(""); }
 	
-lstView_iosItem_(LVM_SETITEMW, LVM_INSERTITEMW, LVITEMW);
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const WCHAR* text) {
-	LVITEMW lvi; lvi.mask = LVIF_TEXT; lvi.pszText = (WCHAR*)text; 
-	lstView_iosItem(hList, item, subItem, &lvi); }
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const WCHAR* text, LPARAM data) {
-	LVITEMW lvi; lvi.mask = LVIF_TEXT|LVIF_PARAM; lvi.pszText = (WCHAR*)text; lvi.lParam = data;
-	lstView_iosItem(hList, item, subItem, &lvi); }
-void WINAPI lstView_iosInt(HWND hList, int item, int subItem, int val) { char buff[32];
-	sprintf(buff, "%d", val); lstView_iosText(hList, item, subItem, buff); }
-void WINAPI lstView_iosInt(HWND hList, int item, int subItem, int val, LPARAM data) { char buff[32];
-	sprintf(buff, "%d", val); lstView_iosText(hList, item, subItem, buff, data); }
-void WINAPI lstView_setData(HWND hList, int item, LPARAM data) { LVITEMW lvi; 
-	lvi.mask = LVIF_PARAM; lvi.lParam = data; lstView_iosItem(hList, item, 0, &lvi); }
+int WINAPI lstView_iosText(HWND hList, int item, 
+	const WCHAR* text, int iSub, LVITEMW* lvi, int mask)
+{
+	lvi->mask = mask; lvi->pszText = (WCHAR*)text;
+	int msg = LVM_SETITEMW; if(!(lvi->iSubItem = iSub)){
+		u32 count = sendMessage(hList, LVM_GETITEMCOUNT);
+		if(item >= count) { item = min(item & INT_MAX, count);
+			msg = LVM_INSERTITEMW; }} lvi->iItem = item;
+	ARGFIX(hList); sendMessageL(hList, msg, (LPARAM)lvi); return item;
+}
+
+int WINAPI lstView_iosText(HWND hList, int item, const char* text,
+	int iSub, LVITEMA* lvi, int mask)
+{
+	SCOPE_EXIT(free(lvi->pszText));	return lstView_iosText(hList,
+	item, utf816_dup(text).data, iSub, (LVITEMW*)lvi, mask);
+}
+
+#define _LSTVW_I1_(CT, LT) \
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text) { \
+	LT lvi; return lstView_iosText(hList, item, text, 0, &lvi, LVIF_TEXT); } \
+int WINAPI lstView_iosText(HWND hList, int item, int iSub, const CT* text) {\
+	LT lvi; return lstView_iosText(hList, item, text, iSub, &lvi, LVIF_TEXT); }\
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text, LPARAM data) { \
+	LT lvi; lvi.lParam = data; \
+	return lstView_iosText(hList,item, text, 0, &lvi, LVIF_TEXT|LVIF_PARAM); }\
+int WINAPI lstView_iosText(HWND hList, int item, const CT* text, LPARAM data, int img) { \
+	LT lvi; lvi.lParam = data; lvi.lParam = data; lvi.iImage = img; \
+	return lstView_iosText(hList, item, text, 0, &lvi, LVIF_TEXT|LVIF_PARAM|LVIF_IMAGE); }
+_LSTVW_I1_(char, LVITEMA); _LSTVW_I1_(WCHAR, LVITEMW); 
+	
+void WINAPI lstView_iosInt(HWND hList, int item, int subItem, int val) { WCHAR buff[32];
+	_itow(val, buff, 10); lstView_iosText(hList, item, subItem, buff); }
+void WINAPI lstView_iosInt(HWND hList, int item, int val, LPARAM data) { WCHAR buff[32];
+	_itow(val, buff, 10); lstView_iosText(hList, item, buff, data); }
+void WINAPI lstView_setData(HWND hList, int item, LPARAM data) { LVITEMW lvi; lvi.mask 
+	= LVIF_PARAM; lvi.lParam = data; sendMessageL(hList, LVM_SETITEMW, (LPARAM)&lvi); }
 	
 // listView reading
 bool WINAPI lstView_getItem(HWND hList, int item, int subItem, LVITEMW* lvi) {
@@ -85,14 +108,7 @@ int WINAPI listView_nextSel(HWND hList, int iStart) {
 	return ListView_GetNextItem(hList, iStart, LVIS_SELECTED); }
 void WINAPI listView_setSel(HWND hList, int item, BOOL bSelect) {
 	ListView_SetItemState(hList, item, bSelect ? LVIS_SELECTED : 0, LVIS_SELECTED); }
+int WINAPI listView_getFocus(HWND hList) {
+	return ListView_GetNextItem(hList, -1, LVIS_FOCUSED); }
 void WINAPI listView_setFocus(HWND hList, int item) {
 	ListView_SetItemState(hList, item, LVIS_FOCUSED, LVIS_FOCUSED); }
-	
-// listview helpers (ANSI) - source lines
-lstView_iosItem_(LVM_SETITEMA, LVM_INSERTITEMA, LVITEMA);
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const char* text) {
-	LVITEMA lvi; lvi.mask = LVIF_TEXT; lvi.pszText = (char*)text; 
-	lstView_iosItem(hList, item, subItem, &lvi); }
-void WINAPI lstView_iosText(HWND hList, int item, int subItem, const char* text, LPARAM data) {
-	LVITEMA lvi; lvi.mask = LVIF_TEXT|LVIF_PARAM; lvi.pszText = (char*)text; lvi.lParam = data;
-	lstView_iosItem(hList, item, subItem, &lvi); }
